abb70caf376d6f5b4900dec873d80dc9bddcf6f4
Jasmine Greenaway <paladique@users.noreply.github.com>
2024-04-05 14:13:19 +0000

    basic buttons

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ document.getElementById('funButton').addEventListener('click', function() {
     const funText = document.getElementById('funText');
     funText.innerHTML = '🎉 Congratulations on taking your first step into web development with CS 140! 🎉';
 });
+
+function areaOfSquareSizeTwenty(){
+    let side = 20;
+    let area = side * side;
+    
+    // This does the same thing, area is area^2
+    //Math.pow(side, 2);
+
+    alert("The area of a square with a side of 20 is: " + area);
+}
+
+function areaOfSquareSizeNegativeTwenty()
+{
+    let side = -20;
+
+    if (side <= 0) 
+    {
+        alert("Area must be larger than 0.")
+    }
+    else
+    {
+        let area = side * side;
+        
+        // This does the same thing, area is area^2
+        //Math.pow(side, 2);
+        alert("The area of a square with a side of 20 is: " + area);
+    }
+}
