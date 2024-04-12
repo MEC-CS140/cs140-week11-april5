b0c92c6766fd99c00be2e38dc86dacc7afe6364f
Jasmine Greenaway <paladique@users.noreply.github.com>
2024-04-12 04:09:39 +0000

    final notes

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function areaOfSquareSizeNegativeTwenty()
 
     if (side <= 0) 
     {
-        alert("Area must be larger than 0.")
+        alert("Area must be larger than 0.");
     }
     else
     {
@@ -31,4 +31,46 @@ function areaOfSquareSizeNegativeTwenty()
         //Math.pow(side, 2);
         alert("The area of a square with a side of 20 is: " + area);
     }
+}
+
+let p = document.getElementById("existingText");
+console.log(p);
+p.textContent = "This text has been changed!";
+
+function changeFooter()
+{
+    let footerP = document.getElementById("footerParagraph");
+    footerP.textContent = "Footer has been changed!";
+
+}
+
+let clicks = 0;
+
+function clickEvent(){
+    let countText = document.getElementById("clicksText");
+    clicks++;
+
+    countText.textContent = clicks;
+    console.log("Number of clicks: " + clicks);
+    
+    console.log("Remainder of clicks divided by 2: " + clicks % 2);
+
+    if (clicks % 2 == 0 ) {
+         alert(clicks + " is even");
+    }
+
+    // console.log("this is a test");
+}
+
+////////////////////Extra: Textboxes
+
+function getText() 
+{
+    let t = document.getElementById("mytextbox");
+    console.log(t.value);
+}
+
+function getCheck(){
+    let c = document.getElementById("mycheckbox");
+    console.log(c.checked);
 }
